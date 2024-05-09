7ed9dc55defed5d0b8fcf08a43666559ef1e20b2
VanyaMark <markova.vanya@gmail.com>
2024-05-09 22:02:29 +0200

    working on logic

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ let numIntentos = 10;
 
 // Actualizar el innerHTML del nodo adecuado con el número de intentos iniciales
 
+const remainingGuesses = document.querySelector('.lastResult');
+remainingGuesses.innerHTML = numIntentos;
+let userGuess = document.getElementById('guessField')
+
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault(); // Impedimos que el formulario haga un petición GET
-
+  console.log(userGuess.value)
   // Comprobar si el número que ha puesto el usuario es mayor o menor que el numeroCorrecto. Tomar decisiones
+
 });
